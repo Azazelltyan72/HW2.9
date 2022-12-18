2b4c1d071e5f55fa8a70fe630e78e36ca0ee04a4
azazell <azaliya_tagirova72@mail.ru>
2022-12-18 22:55:55 +0600

    Домашнее задание 1 сделано

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Set<Product> products = new HashSet<>();
+
+        Product bananas = new Product("Банан", 20.99, 1);
+        Product tomatos = new Product("Помидор", 20.99, 2);
+        Product potatos = new Product("Картошка", 20.99, 1);
+        Product eggs = new Product("Яйца", 20.99, 1);
+        Product apples = new Product("Яблоки", 20.99, 1);
+
+        bananas.addProduct(products);
+        tomatos.addProduct(products);
+        potatos.addProduct(products);
+        eggs.addProduct(products);
+        apples.addProduct(products);
+//        apples.addProduct(products);
+        System.out.println(products.size());
+        System.out.println("В списке продуктов имеются " + Arrays.toString(products.toArray()));
     }
 }
